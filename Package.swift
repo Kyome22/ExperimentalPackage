b7aa3d5d6598cb03bbcf3e3fc87a9699f46ed00d
Takuto NAKAMURA (Kyome) <takuto-nakamura@cybozu.co.jp>
2024-09-11 14:32:50 +0900

    first commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "ExperimentalPackage",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "ExperimentalPackage",
+            targets: ["ExperimentalPackage"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ExperimentalPackage",
+            dependencies: [
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            ]
+        ),
+        .testTarget(
+            name: "ExperimentalPackageTests",
+            dependencies: ["ExperimentalPackage"]
+        ),
+    ]
+)
